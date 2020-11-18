ae9473e6747d2e6f22565315dec0a7e58879b497
dustin-wilson <36450611+dustin-wilson@users.noreply.github.com>
2020-11-18 14:12:34 -0600

    Connect to distance sensor with multiplexer

---
+++ test_directory/DistanceSensorWMulti.ino
@@ -0,0 +1,47 @@
+#include "Adafruit_VL53L0X.h"
+#define TCAADDR 0x70
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL53L0X test");
+  tcaselect(1);
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+}
+
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  Serial.print("Reading a measurement... ");
+  tcaselect(1);
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+    
+  delay(100);
+}
